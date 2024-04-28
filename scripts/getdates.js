57a34a5b2a0ff9366fbcc35ccae915502b0436cb
vidrunodev <brunoch20@hotmail.com>
2024-04-27 23:51:37 -0500

    Assignmet: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var lastModifiedDate = new Date(document.lastModified);
+    var formattedDate = lastModifiedDate.toLocaleDateString();
+    var footerParagraph = document.getElementById("lastmodified");
+    footerParagraph.textContent = "Last modified: " + formattedDate;
+}
