1ab7eee1490650e41205a26d2d11aff6a1b13e45
vidrunodev <brunoch20@hotmail.com>
2024-06-01 22:43:27 -0500

    modified html structure and js to avoid 405 error

--- project/scripts/form.js
+++ project/scripts/form.js
@@ -1,4 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission to avoid 405 not allowed error
+        window.location.href = 'thanks.html'; // Redirect to the thank you page
+    });
+
     document.getElementById("currentYear").textContent = new Date().getFullYear();
     document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
 });
